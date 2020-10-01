1e55729985ae0c9ef7abfaaa7337bbd3861b8cab
Konstantin Fomin <jormun@iMac-Konstantin.local>
2020-10-01 21:42:13 +0300

    add cocoapods

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Konstantin Fomin on 01.10.2020.
+//  Copyright © 2020 Konstantin Fomin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
